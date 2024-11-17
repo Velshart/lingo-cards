25b630e5fee8f2abaaa82188870a7a88ec51ef18
Velshh <pkot2003@gmail.com>
2024-11-17 16:24:57 +0100

    Initial commit

---
+++ src/main/java/me/mmtr/lingocards/LingoCardsApplication.java
@@ -0,0 +1,13 @@
+package me.mmtr.lingocards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoCardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingoCardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mmtr/lingocards/LingoCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mmtr.lingocards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoCardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
